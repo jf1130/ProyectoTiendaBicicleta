4d61587853f8bd7c519e93e77342adf7dcf350d0
Matthewov <100739952+Matthewov@users.noreply.github.com>
2022-05-24 00:03:16 -0500

    producto, enums, accesorio

--- Accesorio.py
+++ Accesorio.py
@@ -1,13 +1,14 @@
 from pickle import TRUE
-
+from Enums import EnumColor
 from Producto import Producto
 
 class Accesorio(Producto):
     
 
-    def __init__(self, Nombre:str, CodSerie:str, Precio: float, Marca: str, EnStock: bool,TamanioRin:float):
+    def __init__(self, Nombre:str, CodSerie:str, Precio: float, Marca: str, EnStock: bool,TamanioRin:float,Color:EnumColor):
         Producto.__init__(self, Nombre, CodSerie , Precio , Marca , EnStock)
         self.TamanioRin=TamanioRin
+        self.Color=Color
         
             
     
@@ -18,9 +19,9 @@ class Accesorio(Producto):
         self.TamanioRin = TamanioRin
 
     def __str__(self):
-        result = f"Nombre del Accesorio: {str(self.Nombre)}\nCodSerie: {str(self.CodSerie)}\nPrecio: {float(self.Precio)}\nMarca: {str(self.Marca)}\nEnstock: {str(self.EnStock)}"
+        result = f"Nombre del Accesorio: {str(self.Nombre)}\nCodSerie: {str(self.CodSerie)}\nPrecio: {float(self.Precio)}\nMarca: {str(self.Marca)}\nEnstock: {str(self.EnStock)}\nColor: {str(self.Color.value)}"
         return result
-objAccesorio= Accesorio("Bici","A12A",3600,"GADO",True,27.5)
+objAccesorio= Accesorio("Bici","A12A",3600,"GADO",True,27.5,EnumColor.Dorado)
 print(objAccesorio)
 
 
--- Equipamento.py
+++ Equipamento.py
@@ -1,14 +1,15 @@
-
+from Enums import EnumTipoEquipamento,EnumTipoTela,EnumTalla
+from Producto import Producto
 
 class Equipamento(Producto):
     
 
-    def __init__(self,Talla:EnumTalla,TipoTela:EnumTipoTela,TipoEquipamento:EnumTipoEquipamento):
+    def __init__(self, Nombre:str, CodSerie:int, Precio:float, Marca:str, EnStock:bool,Talla:EnumTalla,TipoTela:EnumTipoTela,TipoEquipamento:EnumTipoEquipamento):
+        Producto.__init__(self, Nombre, CodSerie, Precio, Marca, EnStock)
         self.Talla=Talla
         self.TipoTela= TipoTela
         self.TipoEquipamento= TipoEquipamento
     
-        Producto.__init__(self, Nombre, CodSerie, Precio, Marca, EnStock)
 
     def GetTalla(self):
         return self.Talla
@@ -27,3 +28,5 @@ class Equipamento(Producto):
     
     def SetValor(self, TipoEquipamento):
         self.TipoEquipamento = TipoEquipamento
+    
+    def
