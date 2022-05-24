46a73c8dedbe74cba48e4b1a7ad4382cbe1c1064
Matthewov <100739952+Matthewov@users.noreply.github.com>
2022-05-24 00:37:45 -0500

    actualizacion objeto Equipamento

--- Equipamento.py
+++ Equipamento.py
@@ -4,7 +4,7 @@ from Producto import Producto
 class Equipamento(Producto):
     
 
-    def __init__(self, Nombre:str, CodSerie:int, Precio:float, Marca:str, EnStock:bool,Talla:EnumTalla,TipoTela:EnumTipoTela,TipoEquipamento:EnumTipoEquipamento):
+    def __init__(self, Nombre:str, CodSerie:str, Precio:float, Marca:str, EnStock:str,Talla:EnumTalla,TipoTela:EnumTipoTela,TipoEquipamento:EnumTipoEquipamento):
         Producto.__init__(self, Nombre, CodSerie, Precio, Marca, EnStock)
         self.Talla=Talla
         self.TipoTela= TipoTela
@@ -29,4 +29,9 @@ class Equipamento(Producto):
     def SetValor(self, TipoEquipamento):
         self.TipoEquipamento = TipoEquipamento
     
-    def
+    def __str__(self):
+        result = f"Nombre Del Equipamento: {str(self.Nombre)}\nCodSerie:{str(self.CodSerie)}\nPrecio:{float(self.Precio)}\nMarca:{str(self.Marca)}\nEnStock:{str(self.EnStock)}\nTalla:{str(self.Talla.value)}\nTipoTela:{str(self.TipoTela.value)}\nTipoEquipamneto:{str(self.TipoEquipamento.value)}"
+        return result
+objEquipamento= Equipamento("Bici","B420","50.000","Llanero",True,EnumTalla.L,EnumTipoTela.Thermoflex,EnumTipoEquipamento.DownHill)
+print(objEquipamento)
+
